0af8ddce9e5eb695cc7d102472caeae3f4c27d60
Justin Camden <justin@geistnet.com>
2020-05-10 13:24:19 -0700

    Copy in working project

---
+++ Source/Asgard/AsgardGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AsgardGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASGARD_API AAsgardGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
